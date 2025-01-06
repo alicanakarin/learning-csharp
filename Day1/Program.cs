f9dde6f59d07315bdcdb561e5fba4942c03cb0e6
AlicanAkarin <alicanakarin@gmail.com>
2025-01-06 17:17:22 +0300

    İkinci gün

---
+++ Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+
+// Change the appearence
+Console.Title = "C# Learning Phase";
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WindowHeight = 30;
+// Get a conversation going
+    Console.WriteLine("Hello, What's Your Name?");
+
+    Console.ReadLine();
+// \n is a new line. you dont need to use extra writeline.
+    Console.WriteLine("Nice too meet you, \nMy name is yokedici31 and I'm learning C# right now.");
+    Console.WriteLine("What's your favorite color?");
+    Console.ReadLine();
+    Console.WriteLine("Cool, Mine is blue.");
+    Console.ReadKey();
---
+++ Day2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "The story of the knight";
+
+Console.WriteLine("A proud knight name...");
+Console.ReadLine();
+Console.WriteLine("...Walked into a bar. At the counter he met a...");
+Console.ReadLine();
+Console.WriteLine("...who asked him what he wanted to drink?");
+Console.ReadLine();
+Console.WriteLine("...shouted the knight! The bartender sushed him quickly.");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("But it was too late... A Dragon ate them both.");
+Console.ReadKey();
